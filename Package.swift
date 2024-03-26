e04716eefd00182a86c1ad7b2e4ecd3e38dbf329
hanna <hanna@dudes-MacBook-Pro.local>
2024-03-26 13:04:50 +0100

    Added package specs

---
+++ Package.swift
@@ -0,0 +1,16 @@
+import PackageDescription
+let package = Package(
+    name: "usersnapMobileSDK",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "usersnapMobileSDK", 
+            targets: ["usersnapMobileSDK"])
+    ],
+    targets: [
+        .binaryTarget(
+            name: "usersnapMobileSDK", 
+            path: "usersnapMobileSDK.xcframework")
+    ])
